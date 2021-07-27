219a5c819a19b01189e7214f7305078af77bb95f
Anand Kumar <anand.karani@leanix.net>
2021-07-27 16:33:40 +0530

    Adding 3rd party package

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -8,8 +9,31 @@ class HomePage extends StatelessWidget {
         title: Text("HomePage"),
       ),
       body: Center(
-        child: Text("Hello"),
+        child: ElevatedButton(
+          child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+              child: Text("Toast")),
+          onPressed: () {
+            _showToastMessage();
+          },
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all<Color>(const Color(0xff1666ee)),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32)))),
+        ),
       ),
     );
+  }
+
+  _showToastMessage() {
+    Fluttertoast.showToast(
+        msg: "This is Center Short Toast",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 }
